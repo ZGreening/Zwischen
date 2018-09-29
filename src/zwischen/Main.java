ef7dd8275afaa27597132bcdbab5e1f7d02dad32
zgreening <zachary.greening@comcast.net>
2018-09-29 11:48:54 -0400

    Formatted for Google Style Java Renamed package Removed Default Controller and fxml files Added documentation to Main.java

---
+++ src/zwischen/Main.java
@@ -0,0 +1,26 @@
+///////////////////////////////////////////////////////////////////////////////
+// Project:     Zwischen
+// File:        Main.java
+// By:          Zachary Greening
+// Group:       3
+// Date:        September 29, 2018
+// Description: Driver Class for the program Zwischen
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package zwischen;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+  }
+
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
