4ce70c143fbe7a54874200490dce17d0f8a55240
Tmarlow98 <32168250+Tmarlow98@users.noreply.github.com>
2018-10-08 11:45:53 -0400

    create account page

---
+++ Main.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        Application.launch(Main.class, args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Create Account Page");
+        primaryStage.setScene(new Scene(root, 600, 550));
+        primaryStage.show();
+    }
+
+
+
+}
