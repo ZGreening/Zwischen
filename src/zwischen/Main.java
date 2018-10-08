d0c8e9e52e4fb0c907c43b1ac83970bb2ca67457
zgreening <zachary.greening@comcast.net>
2018-10-08 18:20:27 -0400

    Added Main Screen Test driver

--- src/zwischen/Main.java
+++ src/zwischen/Main.java
@@ -11,12 +11,22 @@
 package zwischen;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+
+    Scene mainScene = new Scene(root);
+
+    stage.setScene(mainScene);
+
+    stage.show();
   }
 
 
