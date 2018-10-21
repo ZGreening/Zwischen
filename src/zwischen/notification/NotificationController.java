2d6d96e8b231a47a9c35e4e442c1322970cec6f9
Brandon <btdavis3378@eagle.fgcu.edu>
2018-10-20 22:01:22 -0400

    Notification window and controller for buttons.

---
+++ src/zwischen/notification/NotificationController.java
@@ -0,0 +1,56 @@
+package zwischen.notification;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class NotificationController {
+
+  @FXML
+  private ResourceBundle resources;
+
+  @FXML
+  private URL location;
+
+  @FXML
+  void pressAccept(MouseEvent event) {
+
+  }
+
+  @FXML
+  void pressDecline(MouseEvent event) {
+
+  }
+
+  @FXML
+  void pressDelete(MouseEvent event) {
+
+  }
+
+  @FXML
+  void pressDeletedButton(ActionEvent event) {
+
+  }
+
+  @FXML
+  void pressNewButton(ActionEvent event) {
+
+  }
+
+  @FXML
+  void pressRead(MouseEvent event) {
+
+  }
+
+  @FXML
+  void pressReadButton(ActionEvent event) {
+
+  }
+
+  @FXML
+  void initialize() {
+
+  }
+}
