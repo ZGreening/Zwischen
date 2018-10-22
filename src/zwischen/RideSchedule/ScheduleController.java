3467138079544aaf2dcd841cb42ad8f3a8b5a22f
Brandon <btdavis3378@eagle.fgcu.edu>
2018-10-22 18:49:38 -0400

    Ride schedule window outline

---
+++ src/zwischen/RideSchedule/ScheduleController.java
@@ -0,0 +1,50 @@
+package zwischen.RideSchedule;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ScheduleController {
+
+  @FXML
+  private ResourceBundle resources;
+
+  @FXML
+  private URL location;
+
+  @FXML
+  void pressFriend(ActionEvent event) {
+
+  }
+
+  @FXML
+  void pressMM(ActionEvent event) {
+
+  }
+
+  @FXML
+  void pressNotification(ActionEvent event) {
+
+  }
+
+  @FXML
+  void pressProfile(ActionEvent event) {
+
+  }
+
+  @FXML
+  void pressRequestRide(ActionEvent event) {
+
+  }
+
+  @FXML
+  void selectDate(ActionEvent event) {
+
+  }
+
+  @FXML
+  void initialize() {
+
+  }
+}
