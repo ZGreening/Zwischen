48b65d1f5f4882b2e137f627d586f153102f90b1
zgreening <zachary.greening@comcast.net>
2018-10-23 15:04:14 -0400

    Updated

--- src/zwischen/Main.java
+++ src/zwischen/Main.java
@@ -23,7 +23,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage stage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("mainscreen/MainScreen.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("mainscreen/mainScreen.fxml"));
 
     Scene mainScene = new Scene(root);
 
