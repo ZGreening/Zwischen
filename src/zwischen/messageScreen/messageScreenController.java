f203acdbfd44e8b1f22d53fc6cc36e9906a0b121
gagekettering <gdkettering2062@eagle.fgcu.edu>
2018-10-23 14:44:51 -0400

    Adding friendsList and messageScreen

--- src/zwischen/messageScreen/messageScreenController.java
+++ src/zwischen/messageScreen/messageScreenController.java
@@ -1,4 +1,30 @@
 package zwischen.messageScreen;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class messageScreenController {
+
+  @FXML
+  private ResourceBundle resources;
+
+  @FXML
+  private URL location;
+
+  @FXML
+  void backToMainScreen(ActionEvent event) {
+
+  }
+
+  @FXML
+  void newMessage(ActionEvent event) {
+
+  }
+
+  @FXML
+  void initialize() {
+
+  }
 }
