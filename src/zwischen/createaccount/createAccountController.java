5081e1f578451a33ca2fa6faac269a5776947458
zgreening <zachary.greening@comcast.net>
2018-10-23 23:36:24 -0400

    Edited Controller and allow windows to interact with each other

--- src/zwischen/createaccount/createAccountController.java
+++ src/zwischen/createaccount/createAccountController.java
@@ -1,42 +1,97 @@
 package zwischen.createaccount;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import java.awt.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class createAccountController {
+public class CreateAccountController {
 
-    @FXML
-    private PasswordField Password;
+  @FXML
+  private ResourceBundle resources;
 
-    @FXML
-    private TextField UserName;
+  @FXML
+  private URL location;
 
-    @FXML
-    private PasswordField ConfirmPassword;
+  @FXML
+  private PasswordField Password;
 
-    @FXML
-    private TextField Email;
+  @FXML
+  private TextField UserName;
 
-    @FXML
-    private TextField PhoneNumber;
+  @FXML
+  private PasswordField ConfirmPassword;
 
-    @FXML
-    private Button createAccount;
+  @FXML
+  private TextField Email;
 
-    @FXML
-    private Button backToHomePage;
+  @FXML
+  private TextField PhoneNumber;
 
-    @FXML
-    void goToHomePage(ActionEvent event) {
+  @FXML
+  private AnchorPane createAccount;
 
-    }
+  @FXML
+  void onBackToHomepagePressed(ActionEvent event) throws Exception {
+    //todo should this be "homepage" or "loginpage"?
 
-    @FXML
-    void initializeAccountInfo(ActionEvent event) {
+    Stage stage = (Stage) createAccount.getScene().getWindow();
 
-    }
+    stage.close();
 
+    Parent root = FXMLLoader.load(
+        getClass().getClassLoader().getResource("zwischen/loginpage/LoginPage.fxml"));
+
+    Scene scene = new Scene(root);
+
+    stage = new Stage();
+
+    stage.setTitle("Zwischen");
+
+    stage.setScene(scene);
+
+    stage.show();
+  }
+
+  @FXML
+  void onCreateYourAccountPressed(ActionEvent event) throws Exception {
+    Stage stage = (Stage) createAccount.getScene().getWindow();
+
+    stage.close();
+
+    Parent root = FXMLLoader.load(
+        getClass().getClassLoader().getResource("zwischen/mainscreen/MainScreen.fxml"));
+
+    Scene scene = new Scene(root);
+
+    stage = new Stage();
+
+    stage.setTitle("Zwischen");
+
+    stage.setScene(scene);
+
+    stage.show();
+  }
+
+  @FXML
+  void initialize() {
+    assert Password
+        != null : "fx:id=\"Password\" was not injected: check your FXML file 'CreateAccount.fxml'.";
+    assert UserName
+        != null : "fx:id=\"UserName\" was not injected: check your FXML file 'CreateAccount.fxml'.";
+    assert ConfirmPassword
+        != null : "fx:id=\"ConfirmPassword\" was not injected: check your FXML file 'CreateAccount.fxml'.";
+    assert Email
+        != null : "fx:id=\"Email\" was not injected: check your FXML file 'CreateAccount.fxml'.";
+    assert PhoneNumber
+        != null : "fx:id=\"PhoneNumber\" was not injected: check your FXML file 'CreateAccount.fxml'.";
+
+  }
 }
