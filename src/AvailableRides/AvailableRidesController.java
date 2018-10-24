5d92c59b516c791d85243cc69fae282484303570
ChuckT95 <cftaylor7128@eagle.fgcu.edu>
2018-10-24 16:56:33 -0400

    Hopefully final fix

---
+++ src/AvailableRides/AvailableRidesController.java
@@ -0,0 +1,57 @@
+package AvailableRides;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+
+public class AvailableRidesController {
+
+  @FXML
+  private Button Checkallboxes;
+
+  @FXML
+  private Button uncheckAllBoxes;
+
+  @FXML
+  private CheckBox rideRequestCheckBox;
+
+  @FXML
+  private CheckBox rideRequestCheckbox2;
+
+  @FXML
+  private CheckBox riderequestcheckbox3;
+
+  @FXML
+  private Button submissionButton;
+
+  @FXML
+  private Button cancelButton;
+
+  @FXML
+  void cancelAndReturnToMainScreen(ActionEvent event) {
+
+  }
+
+  @FXML
+  void check(ActionEvent event) {
+
+  }
+
+  @FXML
+  void checkAll(ActionEvent event) {
+
+  }
+
+  @FXML
+  void submitAndReturn(ActionEvent event) {
+
+  }
+
+  @FXML
+  void uncheckALL(ActionEvent event) {
+
+  }
+
+}
+
---
+++ src/RideHistory/RideHistoryScreenController.java
@@ -0,0 +1,36 @@
+package RideHistory;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class RideHistoryScreenController {
+
+  @FXML
+  private Button Messege1;
+
+  @FXML
+  private Button messege2;
+
+  @FXML
+  private Button Messege3;
+
+  @FXML
+  private Button MainReturn;
+
+  @FXML
+  void MessegeUser(ActionEvent event) {
+
+  }
+
+  @FXML
+  void messegeUser(ActionEvent event) {
+
+  }
+
+  @FXML
+  void returnToMainScreen(ActionEvent event) {
+
+  }
+
+}
