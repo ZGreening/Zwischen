603e4641eb1ef892a1388b91cb5e27cd7672f310
zgreening <zachary.greening@comcast.net>
2018-11-08 12:26:16 -0500

    Redid LoginPage.fxml to fix bug Added Feedback label to LoginPage

--- src/loginpage/LoginController.java
+++ src/loginpage/LoginController.java
@@ -10,12 +10,16 @@ package loginpage;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import other.Globals;
 
 public class LoginController {
+
+  @FXML
+  private Label feedbackLabel;
 
   @FXML
   private PasswordField password;
@@ -34,6 +38,12 @@ public class LoginController {
   @FXML
   void onLoginPressed(ActionEvent event) {
     //Todo give user feedback on why it didn't login
+    //Demonstration of feedback label
+    if (username.getText().isEmpty()) {
+      feedbackLabel.setText("UserName is Empty");
+      return;
+    }
+
     Globals.changeScene("mainscreen/mainScreen.fxml", root);
   }
 }
