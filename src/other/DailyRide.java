68f17fcd4dc7665d5dd0b6214b559e9d8d82dd24
zgreening <zachary.greening@comcast.net>
2018-11-28 18:52:34 -0500

    Added DailyRide global class

---
+++ src/other/DailyRide.java
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////
+// Project:     Zwischen
+// File:        DailyRide.java
+// Group:       3
+// Date:        November 28, 2018
+// Description: Ride class to store information of a ride.
+///////////////////////////////////////////////////////////////////////////////
+
+package other;
+
+public class DailyRide {
+
+  boolean isAvailable;
+  String time;
+  String pickupLocation;
+  String destination;
+
+  public boolean isAvailable() {
+    return isAvailable;
+  }
+
+  public void setAvailable(boolean available) {
+    isAvailable = available;
+  }
+
+  public String getTime() {
+    return time;
+  }
+
+  public void setTime(String time) {
+    this.time = time;
+  }
+
+  public String getPickupLocation() {
+    return pickupLocation;
+  }
+
+  public void setPickupLocation(String pickupLocation) {
+    this.pickupLocation = pickupLocation;
+  }
+
+  public String getDestination() {
+    return destination;
+  }
+
+  public void setDestination(String destination) {
+    this.destination = destination;
+  }
+}
