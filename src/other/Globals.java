dfa8fb6df4779cc6ad991d844efe3a6092847b4e
zgreening <zachary.greening@comcast.net>
2018-12-04 16:39:59 -0500

    Updated from master branch

--- src/other/Globals.java
+++ src/other/Globals.java
@@ -17,6 +17,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -29,16 +31,33 @@ import messages.MessagesController;
 
 public class Globals {
 
-  private static final ArrayList<User> availableDrivers = new ArrayList<>();
   private static final User currentUser = new User();
-  public static Request currentRequest = new Request();
+
+  private static final ObservableList<String> timeList = FXCollections
+      .observableArrayList("7:00 AM", "7:30 AM", "8:00 AM", "8:30 AM", "9:00 AM",
+          "9:30 AM", "10:00 AM", "10:30 AM", "11:00 AM", "11:30 AM", "12:00 PM", "12:30 PM",
+          "1:00 PM", "1:30 PM", "2:00 PM", "2:30 PM", "3:00 PM", "3:30 PM", "4:00 PM", "4:30 PM",
+          "5:00 PM", "5:30 PM", "6:00 PM", "6:30 PM", "7:00 PM", "7:30 PM", "8:00 PM",
+          "8:30 PM", "9:00 PM", "9:30 PM", "10:00 PM", "10:30 PM", "11:00 PM", "11:30 PM",
+          "12:00 AM", "12:30 AM", "1:00 AM", "1:30 AM", "2:00 AM", "2:30 AM", "3:00 AM", "3:30 AM",
+          "4:00 AM", "4:30 AM", "5:00 AM", "5:30 AM", "6:00 AM", "6:30 AM");
+
+  private static final ObservableList<String> locationList = FXCollections
+      .observableArrayList("Coastal Village Apartments", "Coconut Point Mall",
+          "Florida Gulf Coast University", "Florida SouthWestern State College",
+          "Gulf Coast Town Center", "Miromar Outlets", "The Reef Apartments",
+          "Walmart Supercenter (Estero)");
+
+  public static ObservableList<String> getTimeList() {
+    return timeList;
+  }
+
+  public static ObservableList<String> getLocationList() {
+    return locationList;
+  }
 
   public static User getCurrentUser() {
     return currentUser;
-  }
-
-  public static ArrayList<User> getAvailableDrivers() {
-    return availableDrivers;
   }
 
   /**
